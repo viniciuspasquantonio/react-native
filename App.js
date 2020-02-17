8ddc3760c634298eedb487e018c47150886b4c09
Vinicius Pasquantonio <vinicius.pasquantonio@agido.com>
2020-02-17 22:43:03 +0100

    TextInput

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { statement } from '@babel/template';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>neira fff</Text>
-    </View>
-  );
+export default class App extends Component{
+  
+  state = {
+    placeName:""
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput  
+          style={{width: 300, borderColor:"black", borderWidth:1}}
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
