ade3e47da17dcace95e3e700b0a6892eac788835
Vinicius Pasquantonio <vinicius.pasquantonio@agido.com>
2020-02-23 22:41:41 +0100

    divide it in components, using scroll view and reacting to Press Events

--- App.js
+++ App.js
@@ -1,56 +1,43 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { statement } from '@babel/template';
-import ListItem from './src/components/ListItem/ListItem';
+import PlaceList from './src/components/PlaceList/PlaceList';
+import PlaceInput from  './src/components/PlaceInput/PlaceInput';
 export default class App extends Component{
   
   state = {
-    placeName:"",
     places: []
   };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
 
-  placeSubmitHandler = () => {
-    if(this.state.placeName.trim() === ""){
-      return;
-    }
-
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return  {
-        places:prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
       };
     });
-  };
+  }
+
+  placeDeleteHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place,i) =>{
+            return i !== index;
+        })
+      };
+    });
+  }
 
   render() {
-    const placesOutput = this.state.places.map((place,i) => (
-      <ListItem 
-        key={i} 
-        placeName={place} 
-        onItemPressed={()=> alert("item pressed, Id:"+i)}
-      />
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="An awseome place"  
-            style={styles.placeInput}
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangedHandler}
-          />
-          <Button
-            title="Add" 
-            style={styles.placeButton}
-            onPress={this.placeSubmitHandler}
-          />
-        </View>
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <PlaceInput 
+          onPlaceAdded={this.placeAddedHandler}
+        />
+        <PlaceList 
+          places={this.state.places}
+          onItemDeleted={this.placeDeleteHandler}
+        />
       </View>
     );
   }
@@ -63,20 +50,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     padding:20
-  },
-  inputContainer:{
-    flexDirection: 'row',
-    justifyContent:'space-between',
-    alignItems:"center",
-    width:"100%"
-  },
-  placeInput:{
-    width: '70%'
-  },
-  placeButton:{
-    width: '30%'
-  },
-  listContainer: {
-    width: "100%"
   }
 });
