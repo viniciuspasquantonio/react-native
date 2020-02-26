311497fc3b5a1f7752664b5b3dee9ca682962b30
Vinicius Pasquantonio <vinicius.pasquantonio@agido.com>
2020-02-26 21:08:33 +0100

    Connection React Native to Redux

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { AppRegistry, Platform } from 'react-native';
-import {Provider} from './react-redux';
+import {Provider} from 'react-redux';
 import App from './App';
-import configureStore from './src/rstore/rconfigureStore'; 
+import configureStore from './src/store/configureStore'; 
 
 const store = configureStore();
 
